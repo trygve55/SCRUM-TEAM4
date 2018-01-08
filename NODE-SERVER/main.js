9ed9c0860ff72e5843498ad80fe5407610450d5c
Trygve Nerland <trygvne@stud.ntnu.no>
2018-01-08 15:37:53 +0100

    Database model updated

--- NODE-SERVER/main.js
+++ NODE-SERVER/main.js
@@ -5,21 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var app = express();
 
-<<<<<<< HEAD
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
-  
-app.start = app.listen = function(){
-	return server.listen.apply(server, arguments)
-}
-
-app.start(80);
-console.log("Server started on port 80");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-=======
->>>>>>> 35305fd316b848becbf0f6e5d4e5d081fed7954a
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('[:date[web]] ":method :url" :status :res[content-length] - :response-time ms'));
