35b652342633f88221dc6eb5943e9414f87a0941
osatel <olav.telneset@gmail.com>
2018-01-09 11:15:36 +0100

    nothing of value was added

--- NODE-SERVER/api/user.js
+++ NODE-SERVER/api/user.js
@@ -2,5 +2,11 @@ var router = require('express').Router();
 
 module.exports = router;
 
+router.post("/", function(req, res){
+    //post username, password, etc.
+});
+
 
 // ---------------------
+
+
