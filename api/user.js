b6cfae5e4131bd287aab81c71f13f85f4692bcb7
Olav <olav.telneset@gmail.com>
2018-01-09 16:02:26 +0100

    added changes

--- api/user.js
+++ api/user.js
@@ -2,15 +2,28 @@ var router = require('express').Router();
 
 module.exports = router;
 
-router.post('/', function(req, res){
+router.post('/regUser', function(req, res){
     console.log('POST-request established');
     pool.getConnection(function(err, connection) {
         if(err) {
             res.status(500);
-            res.json({'Error connecting to database'} + err);
+            res.json({'Error' : 'connecting to database: ' } + err);
             return;
         }
         console.log('Connected to database');
+
+        var username = req.body.username;
+
+        connection.query('SELECT username FROM person WHERE username = ?', [username], function (err, result) {
+            if(err) throw err;
+            if (result) console.log('username exists'); //restrn error
+        });
+        connection.query('SELECT email FROM person WHERE email = ?', [req.body.email], function (err, result) {
+            if(err) throw err;
+            if (result) console.log('email already in use'); /restrn error
+        });
+
+        connection.query('intert into', values, )
     });
 });
 
