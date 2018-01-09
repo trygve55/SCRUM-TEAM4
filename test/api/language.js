099a3be2809a68beef1685a9e3ad0193ac5486e4
Andreas Hammer <andreas.hammer.97@protonmail.com>
2018-01-09 15:40:15 +0100

    test

--- test/api/language.js
+++ test/api/language.js
@@ -5,7 +5,8 @@ var lang = require('../../api/language');
 describe('#language.js', function(){
     it('should return username', function(){
         request.get('/api/language').send({
-
+            path: '/login.html',
+            lang: 'en_US'
         }).expect(200).end(function(err, res){
             console.log(res);
             chai.expect(res).to.equal("Username");
