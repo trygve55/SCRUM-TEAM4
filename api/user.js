ec771919a74135f6fe971ab18e0b772b0bbf376a
Trygve Nerland <trygvne@stud.ntnu.no>
2018-01-10 11:46:25 +0100

    Made regUser async

--- api/user.js
+++ api/user.js
@@ -12,40 +12,45 @@ router.post('/regUser', function(req, res){
         }
         console.log('Connected to database');
 
-        connection.query('SELECT username FROM person WHERE username = ?', [req.body.username], function (err, result) {
+        var user = req.body;
+
+        var emailRegex = new RegExp('/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/');
+        if(!emailRegex.test(user[user.email])) throw err;
+
+        connection.query('SELECT username FROM person WHERE username = ?', [user.username], function (err, result) {
             if (err) throw err;
             if (result) console.log('username exists'); //return error
-        });
 
-        connection.query('SELECT email FROM person WHERE email = ?', [req.body.email], function (err, result) {
-            if (err) throw err;
-            if (result) console.log('email already in use'); //return error
-        });
+            connection.query('SELECT email FROM person WHERE email = ?', [user.email], function (err, result) {
+                if (err) throw err;
+                if (result) console.log('email already in use'); //return error
 
-        var values = [
-            req.body.email,
-            req.body.username,
-            req.body.password,
-            req.body.forename,
-            req.body.middlename,
-            req.body.lastname,
-            req.body.phone_number,
-            req.body.birth_day,
-            req.body.is_verified,
-            req.body.gende,
-            req.body.profile_pic
-        ];
+                var values = [
+                    user.email,
+                    user.username,
+                    user.password,
+                    user.forename,
+                    user.middlename,
+                    user.lastname,
+                    user.phone_number,
+                    user.birth_day,
+                    user.is_verified,
+                    user.gender,
+                    user.profile_pic
+                ];
 
-        var emailRegex = '/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
-        if(!emailRegex.test(values[req.body.email])) throw err;
+                connection.query('INSERT INTO person ' +
+                    '(email, username, password_hash, forename, middlename,' +
+                    'lastname, phone_number, birth_day, is_verified,' +
+                    'gender, profile_pic) VALUES(?,?,?,?,?,?,?,?,?,?,?)', values, function(err, result) {
+                    if (err) throw err;
+                    if (result) console.log(result);
+                    connection.release();
 
-        connection.query('INSERT INTO person ' +
-            '(email, username, password_hash, forename, middlename,' +
-            'lastname, phone_number, birth_day, is_verified,' +
-            'gender, profile_pic) VALUES(?,?,?,?,?,?,?,?,?,?,?)', values, function(err, result) {
-            if (err) throw err;
-            if (result) console.log(result);
-            connection.release();
+                    //svar på post request
+                    //res.
+                });
+            });
         });
     });
 });
