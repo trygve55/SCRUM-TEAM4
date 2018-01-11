be97505b826b9b5c6260e08a3f49a026f2142c60
Olav <olav.telneset@gmail.com>
2018-01-11 11:13:39 +0100

    register new user function complete

--- api/user.js
+++ api/user.js
@@ -12,24 +12,31 @@ router.post('/regUser', function(req, res){
             return;
         }
         console.log('Connected to database');
-
         var user = req.body;
 
+        if(!checkValidUsername(user.username) && !checkValidEmail(user.email)){
+            res.status(400).send("Bad request");
+        }
 
-        console.log("test email");
-        var emailRegex = new RegExp('/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/');
-        //if(!emailRegex.test(user[user.email])) throw err;
+        connection.query('SELECT COUNT(username) AS counted FROM person WHERE username = ?', [user.username], function (err, result) {
+            if(err) {
+                console.log(err);
+            }
 
-        console.log("email ok");
+            if (result[0].counted === 1) {
+                connection.release();
+                return res.status(400).send("Bad request");
+            }
 
-        connection.query('SELECT username FROM person WHERE username = ?', [user.username], function (err, result) {
-            if (err) throw err;
-            if (result) console.log('username exists'); //return error
-            console.log("username");
+            connection.query('SELECT COUNT(email) AS counted FROM person WHERE email = ?', [user.email], function (err, result) {
+                if(err) {
+                    console.log(err);
+                }
 
-            connection.query('SELECT email FROM person WHERE email = ?', [user.email], function (err, result) {
-                if (err) throw err;
-                if (result) console.log('email already in use'); //return error
+                if (result[0].counted === 1) {
+                    connection.release();
+                    return res.status(400).send("Bad request");
+                }
 
                 user.shopping_list_id = 0;
                 auth.hashPassword(user, function (user) {
@@ -42,7 +49,7 @@ router.post('/regUser', function(req, res){
                         user.middlename,
                         user.lastname,
                         user.phone,
-                        new Date(user.birth_date).toISOString().slice(0, 19).replace('T', ' '),
+                        new Date(user.birth_day).toISOString().slice(0, 10),
                         user.gender,
                         user.profile_pic,
                         user.shopping_list_id
@@ -53,10 +60,7 @@ router.post('/regUser', function(req, res){
                         'lastname, phone, birth_date,' +
                         'gender, profile_pic, shopping_list_id) VALUES (?,?,?,?,?,?,?,?,?,?,?)', values, function(err, result) {
                         if (err) throw err;
-                        if (result) console.log(result);
                         connection.release();
-
-                        //svar på post request
                         res.json({message: "true"});
                     });
                 });
@@ -64,6 +68,71 @@ router.post('/regUser', function(req, res){
         });
     });
 });
+
+router.get('/okUser', function (req, res) {
+    var username = req.query.username;
+
+    pool.getConnection(function (err, connection) {
+        if(err) {
+            res.status(500);
+            res.json({'Error' : 'connecting to database: ' } + err);
+            return;
+        }
+
+        if(checkValidUsername(username)){
+            return res.status(400);
+        }
+
+        connection.query('SELECT COUNT(username) AS counted FROM person WHERE username = ?', [username], function (err, result){
+            if(result[0].counted === 1) {
+                connection.release();
+                return res.status(400);
+            } else {
+                connection.release();
+                return res.status(200);
+            }
+        });
+    });
+});
+
+router.get('/okEmail', function (req, res) {
+    var email = req.query.email;
+
+    pool.getConnection(function (err, connection) {
+        if(err) {
+            res.status(500);
+            res.json({'Error' : 'connecting to database: ' } + err);
+            return;
+        }
+
+        if(checkValidEmail(email)){
+            return res.status(400);
+        }
+
+        connection.query('SELECT COUNT(email) AS counted FROM person WHERE email = ?', [email], function (err, result){
+            if(result[0].counted === 1) {
+                connection.release();
+                return res.status(400).send("bad request");
+            } else {
+                connection.release();
+                return res.status(200);
+            }
+        });
+    });
+});
+
+//returns true if valid
+function checkValidUsername(username) {
+    var usernameRegex = /^[a-zA-Z0-9]+$/;
+    return usernameRegex.test(username.toLowerCase());
+}
+//returns true if valid
+function checkValidEmail(email) {
+    console.log(email);
+    var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email.toLowerCase());
+}
+
 
 
 // ---------------------
