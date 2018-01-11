40b4c7e6b251f96f42022ff29041d8c334adba14
Trygve Nerland <trygvne@stud.ntnu.no>
2018-01-11 08:54:48 +0100

    Fixed filenames

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ router.use('/language', require('./api/language'));
 router.use('/login', require('./api/login'));
 router.use('/group', require('./api/group'));
 router.use('/user', require('./api/user'));
-router.use('/shoppingList', require('./api/shopping_list'));
+router.use('/shoppingList', require('./api/shoppingList'));
 router.use('/currency', require('./api/currency'));
 
 module.exports = router;
