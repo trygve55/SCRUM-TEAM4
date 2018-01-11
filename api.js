4621c61e4522e4d14fba6499250626f08983ae75
Trygve Nerland <trygvne@stud.ntnu.no>
2018-01-11 08:19:59 +0100

    Added currency and shopping list API

--- api.js
+++ api.js
@@ -2,5 +2,6 @@ var router = require('express').Router();
 
 router.use('/language', require('./api/language'));
 router.use('/shoppingList', require('./api/shopping_list'));
+router.use('/currency', require('./api/currency'));
 
 module.exports = router;
