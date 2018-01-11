3ec332bb1a040149b34773c7a51b45837e13a81c
Trygve Nerland <trygvne@stud.ntnu.no>
2018-01-11 08:53:52 +0100

    Added shopping list REST calls

--- api/currency.js
+++ api/currency.js
@@ -12,14 +12,17 @@ router.get('/currencies', function(req, res){
         }
         console.log('Connected to database');
 
-        connection.query('SELECT INTO shopping_list ' +
-            '(shopping_list_name) VALUES (?)', [req.body.shopping_list_name], function(err, result) {
-            if (err) throw err;
-            if (result) console.log(result);
+        connection.query('SELECT * FROM currency ', function(err, result) {
             connection.release();
 
+            if(err) {
+                res.status(500);
+                res.json({'Error' : 'connecting to database: ' } + err);
+                return;
+            }
+
             //svar på post request
-            res.json({message: "true"});
+            res.json(result);
         });
     });
 });
