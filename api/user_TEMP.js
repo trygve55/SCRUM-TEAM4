48eeb9eecb9bc354f2d6f4e0e1a24fe6d556ab79
magnueil <magnueil@stud.ntnu.no>
2018-01-12 14:11:38 +0100

    Updated functionality of /api/user_TEMP/getUser, test still needs work

--- api/user_TEMP.js
+++ api/user_TEMP.js
@@ -3,31 +3,84 @@ var router = require('express').Router();
 module.exports = router;
 
 /*
-Returns the requested user's data in an array in the following format:
-[0] email, [1] forename, [2] middle name, [3] surname, [4] phone number, [5] group_ids[]
-TODO test that group_ids are added properly
+Returns the requested information about the requested users. The request body must contain two variables: req.body.variables and
+req.body.users, both arrays. The first one is a list of the variables you'd like to retrieve, while the second is a list of
+user IDs for which you'd like to retrieve those variables' data.
+Sensitive data variables are only available to the current session's user.
+
+Variables available to all logged in users:
+username, forename, middlename, lastname, gender, profile_pic, profile_pic_tiny and last_active
+Variables available to users about themselves:
+email, phone, birth_date, is_verified, shopping_list_id, user_language, user_deactivated, facebook_api_id
+
+Example 1: the client needs to know the full names, gender, and profile_pic_tiny (all public) of some person_ids
+Request body:
+{
+    variables: ['forename', 'middlename', 'lastname', 'gender', 'profile_pic_tiny'],
+    users: [309, 482, 100, 2]
+}
+
+Example 2: the client needs to know the email, phone, and user_language of the currently logged in user
+Request body:
+{
+    variables: ['email', 'phone', 'user_language'],
+    users: [300]
+}
+If the session ID stored on the server matches the requested user's ID, the info is provided. If it does not, or more
+than one ID is provided in the req.body.users, the server will respond with a 403 Forbidden status code, since the
+info is only available to the user with the ID 300, when they are logged in.
  */
 
+var publicVars = ['username', 'forename', 'middlename', 'lastname', 'gender', 'profile_pic',
+    'profile_pic_tiny', 'last_active'];
+var privateVars = ['email', 'phone', 'birth_date', 'is_verified', 'shopping_list_id', 'user_language',
+    'user_deactivated', 'facebook_api_id'];
+
+function reqForPrivateVars(reqVars) {
+    var result = false;
+    reqVars.forEach(function(element) {
+        if(privateVars.indexOf(element) > -1) {
+            result = true;
+            return;
+        }
+    });
+    return result;
+}
+
 router.post('/getUser', function(req, res) { // TODO add authentication
+    if(!req.session.person_id || checkRequestArray(req.body.variables) > 0 ||
+        (reqForPrivateVars(req.body.variables) && (req.body.users.length > 1 || req.session.person_id != req.body.users[0]))) {
+            return res.status(403).send("Forbidden request");
+    }
+    console.log('API: authentication passed');
+    var sqlQuery = 'SELECT ?';
+    for(i = 1; i < req.body.variables.length; i++) {
+        sqlQuery += ',?';
+    }
+    sqlQuery += ' FROM person WHERE person_id = ?';
+    for(i = 1; i < req.body.users.length; i++) {
+        sqlQuery += ' OR person_id = ?';
+    }
+    var values = req.body.variables;
+    req.body.users.forEach(function(element) {
+        values.push(element);
+    });
+
     pool.getConnection(function(err, connection) {
         if(err) {
             res.status(500);
-            res.json({"error:": "Error connecting to database" + err});
+            res.json({"error": "Error connecting to database" + err});
             return;
         }
-        var values = ['empty'];
-        var sql = "SELECT email, forename, middlename, lastname, phone, person_id FROM person WHERE username = ?";
-        connection.query(sql, [req.body.username], function(err, result) {
-            if(err) throw err;
-            values = result;
 
-            sql = "SELECT group_id FROM group_person WHERE person_id = ?";
-            connection.query(sql,[values[5]], function(err, result) {
-                if(err) throw err;
-                delete values.person_id;
-                values.group_ids = result;
-                res.send(values);
-            });
+        connection.query(sqlQuery, values, function(err, result) {
+            if(err) {
+                res.status(500);
+                res.json({"error": "Error in query to database" + err});
+                return;
+            }
+            res.status(200);
+            res.send(result);
         });
     });
 });
@@ -49,6 +102,20 @@ function getUsername(email, con) {
         var values = result;
     });
     return values;
+}
+
+function checkRequestArray(inputArray) {
+    var validInput = ['person_id', 'email', 'username',
+        'forename', 'middlename',
+        'lastname', 'phone', 'birth_date', 'is_verified',
+        'gender', 'profile_pic', 'profile_pic_tiny',
+        'last_active', 'shopping_list_id',
+        'user_language', 'user_deactivated', 'facebook_api_id'];
+    var errors = 0;
+    inputArray.forEach(function(element) {
+        if(validInput.indexOf(element) < 0) errors++;
+    });
+    return errors;
 }
 
 function insertTestPerson(connection) {
