3130444836f8ceb57e6e7d8029cd054f764d5792
Andrz Cabal <andrzejc@stud.ntnu.no>
2018-01-12 10:32:36 +0100

    Adding people to shopping list and fix get method

--- api/shoppingList.js
+++ api/shoppingList.js
@@ -25,15 +25,74 @@ router.post('/', function(req, res){
 });
 
 router.get('/:shopping_list_id', function(req, res){
-    console.log('GET-request established');
-    pool.getConnection(function(err, connection) {
+	console.log('GET-request established');
+	pool.getConnection(function(err, connection) {
 		checkConnectionError(err, connection, res);
 
-        connection.query('SELECT shopping_list_id, shopping_list_name, currency_id ' +
-            'FROM shopping_list WHERE shopping_list_id = ?;', [req.params.shopping_list_id],
-			function(err, result) {checkResult(err, result, connection, res);}
+		connection.query('SELECT * ' +
+			'FROM shopping_list LEFT JOIN currency USING(currency_id) ' +
+			'LEFT JOIN shopping_list_entry USING(shopping_list_id) WHERE ' +
+			'shopping_list.shopping_list_id = ?',
+			[req.params.shopping_list_id],
+			function(err, result) {
+				connection.release();
+				if (err) {throw err;}
+				if (result) {
+					var length = result.length, entries = [];
+					for (i = 0; i < result.length; i++) {
+						entries.push({
+							"shopping_list_entry_id":result[i].shopping_list_entry_id,
+							"entry_text":result[i].entry_text,
+							"added_by_person_id":result[i].added_by_person_id,
+							"purchased_by_person_id":result[i].purchased_by_person_id,
+							"cost":result[i].cost,
+							"datetime_added":result[i].datetime_added,
+							"datetime_purchased":result[i].datetime_purchased
+						});
+					}
+					res.json({
+						"shopping_list_id":result[0].shopping_list_id,
+						"shopping_list_name":result[0].shopping_list_name,
+						"currency_id":result[0].currency_id,
+						"currency_short":result[0].currency_short,
+						"currency_long":result[0].currency_long,
+						"currency_sign":result[0].currency_sign,
+						"currency_major":result[0].currency_major,
+						"currency_long":result[0].currency_long,
+						"shopping_list_entries":entries
+					});
+				}
+			}
 		);
-    });
+	});
+});
+
+router.post('/:shopping_list_id', function(req, res) {
+	var data = req.body;
+	var people = data.persons;	// Array
+	
+	console.log('POST-request initiating');
+	pool.getConnection(function(err, connection) {
+		checkConnectionError(err, connection, res);
+		connection.query(
+			'INSERT INTO shopping_list_person(' +
+			'shopping_list_id, person_id, paid_amount, ' +
+			'invite_accepted, invite_sent_datetime, is_hidden) ' +
+			'SELECT @listID:=?,?,?,?,?,? FROM shopping_list ' +
+			'WHERE NOT EXISTS (SELECT shopping_list.shopping_list_id FROM ' +
+			'home_group,person,shopping_list WHERE person.shopping_list_id = @listID OR ' +
+			'home_group.shopping_list_id = @listID) OR NOT EXISTS ' +
+			'(SELECT person_id FROM shopping_list_person WHERE shopping_list_id = @listID) LIMIT 1;',
+			[
+				req.params.shopping_list_id,
+				data.person_id,
+				data.paid_amount,
+				data.invite_accepted,
+				new Date(data.invite_sent_datetime).toISOString().slice(0, 10),
+				data.is_hidden
+			],
+			function(err, result) {checkResult(err, result, connection, res);});
+	});
 });
 
 router.put('/:shopping_list_id', function(req, res) {
