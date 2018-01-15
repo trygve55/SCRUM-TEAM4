7276a4ea64aa83c9b8eca22fdb870c00181de716
odask <oda.skaug@hotmail.com>
2018-01-15 12:24:27 +0100

    Updates shoppinglist language

--- public/js/shoppinglist.js
+++ public/js/shoppinglist.js
@@ -25,18 +25,27 @@ $('document').ready(function (){
     $('#login-norway').click(function () {
         $.ajax({
             url: '/api/language',
-            method: 'GET',
+            method: 'POST',
             data: {
-                lang: 'nb_NO',
-                path: window.location.pathname
+                lang: 'nb_NO'
             },
             success: function (data) {
-                for (var p in data) {
-                    if (data.hasOwnProperty(p)) {
-                        $("#" + p).html(data[p]);
+                $.ajax({
+                    url: '/api/language',
+                    method: 'GET',
+                    data: {
+                        lang: 'nb_NO',
+                        path: window.location.pathname
+                    },
+                    success: function (data) {
+                        for (var p in data) {
+                            if (data.hasOwnProperty(p)) {
+                                $("#" + p).html(data[p]);
+                            }
+                        }
+                        $("#shoppingcart").attr('title', data["shoppingcart"]);
                     }
-                }
-                $("#shoppingcart").attr('title', data["shoppingcart"]);
+                });
             }
         });
     });
@@ -44,18 +53,27 @@ $('document').ready(function (){
     $('#login-england').click(function () {
         $.ajax({
             url: '/api/language',
-            method: 'GET',
+            method: 'POST',
             data: {
-                lang: 'en_US',
-                path: window.location.pathname
+                lang: 'en_US'
             },
             success: function (data) {
-                for (var p in data) {
-                    if (data.hasOwnProperty(p)) {
-                        $("#" + p).html(data[p]);
+                $.ajax({
+                    url: '/api/language',
+                    method: 'GET',
+                    data: {
+                        lang: 'en_US',
+                        path: window.location.pathname
+                    },
+                    success: function (data) {
+                        for (var p in data) {
+                            if (data.hasOwnProperty(p)) {
+                                $("#" + p).html(data[p]);
+                            }
+                        }
+                        $("#shoppingcart").attr('title', data["shoppingcart"]);
                     }
-                }
-                $("#shoppingcart").attr('title', data["shoppingcart"]);
+                });
             }
         });
     });
