bd1097ebcfef6957d8594df40ababf9fb9c206cf
Andreas Hammer <andreas.hammer.97@protonmail.com>
2018-01-16 15:06:48 +0100

    Added comments

--- api/tasks.js
+++ api/tasks.js
@@ -3,17 +3,25 @@ var router = require('express').Router();
 module.exports = router;
 
 /**
-* The POST request for adding a new task.
-* Required parameters: group_id, todo_text, created_by_id.
-* Optional parameters: datetime_deadline, datetime_done, done_by_id.
+ * Add new task to the group task list
+ *
+ * URL: /api/tasks
+ * method: POST
+ * data: {
+ *      group_id,
+ *      todo_text
+ *
+ *      Optional:
+ *      datetime_deadline,
+ *      datetime_done,
+ *      done_by_id
+ * }
 */
 router.post('/', function(req, res) {
-
-	var data = req.body, done = null;
-	if (data.datetime_done) {done = data.datetime_done;}
-
+	var data = req.body;
 	pool.getConnection(function(err, connection) {
-		if (!checkConnectionError(err, connection, res)) {return;}
+		if (!checkConnectionError(err, connection, res))
+		    return;
 		connection.query(
 			'INSERT INTO todo (' +
 			'group_id, todo_text, datetime_deadline, datetime_done, created_by_id, done_by_id' +
@@ -22,131 +30,144 @@ router.post('/', function(req, res) {
 				checkRange(data.group_id, 1, null),
 				data.todo_text,
 				data.datetime_deadline,
-				done,
+                data.datetime_done,
 				checkRange(req.session.person_id, 1, null),	// data.created_by_id to test this.
 				checkRange(data.done_by_id, 1, null)
-			],
-			function(err, result) {checkResult(err, result, connection, res);}
+			], function(err, result) {
+			    checkResult(err, result, connection, res);
+			}
 		);
 	});
 });
 
 /**
-* The POST request for adding a person to the task.
-* Required parameters: todo_id, person_id.
+ * Add a person to a task
+ *
+ * URL: /api/tasks/person/{todo_id}
+ * method: POST
+ * data: {
+ *      person_id
+ * }
 */
-router.post('/person/', function(req, res) {
-
+router.post('/person/:todo_id', function(req, res) {
 	pool.getConnection(function(err, connection) {
-		if (!checkConnectionError(err, connection, res)) {return;}
-		var data = req.body;
+		if (!checkConnectionError(err, connection, res))
+		    return;
 		connection.query(
 			'INSERT INTO todo_person (todo_id, person_id) VALUES (?, ?);',
-			[checkRange(data.todo_id, 1, null), checkRange(data.person_id, 1, null)],
+			[checkRange(req.params.todo_id, 1, null), checkRange(req.body.person_id, 1, null)],
 			function(err, result) {
-				connection.release();
-				if (err) {throw err;}
-				if (result) {res.status(200).json({success: "Success", id: result.insertId});}
+                if (!checkConnectionError(err, connection, res))
+                    return;
+				res.status(200).json({success: "Success", id: result.insertId});
 			}
 		);
 	});
 });
 
 /**
-* The GET request for getting the task details and all people assigned.
-* Required parameters: todo_id(URL).
+ * Get the data about a task
+ *
+ * URL: /api/tasks/{todo_id}
+ * method: GET
 */
 router.get('/:todo_id', function(req, res) {
-
 	pool.getConnection(function(err, connection) {
-		if (!checkConnectionError(err, connection, res)) {return;}
-
+		if (!checkConnectionError(err, connection, res))
+		    return;
 		connection.query('SELECT * FROM todo LEFT JOIN todo_person USING(todo_id) WHERE todo.todo_id = ?',
-			[req.params.todo_id],
-			function(err, result) {
+			[req.params.todo_id], function(err, result) {
 				connection.release();
-				if (err) {throw err;}
-				if (result) {
-					if (result.length > 0) {
-						var people = [];
-						for (var i = 0; i < result.length; i++) {people.push({"person_id":result[i].person_id});}
-						var values = {};
-						for (var p in result[0]) {values[p] = result[0][p];}
-						delete values.person_id;
-						values.people = people;
-						res.status(200).json(values);
-					}
-					else {res.status(400).json(result);}
-				}
+				if (err)
+				    return res.status(500).send();
+                if (result.length > 0) {
+                    var people = [];
+                    for (var i = 0; i < result.length; i++)
+                        people.push({"person_id":result[i].person_id});
+                    var values = {};
+                    for (var p in result[0])
+                        values[p] = result[0][p];
+                    delete values.person_id;
+                    values.people = people;
+                    res.status(200).json(values);
+                }
+                else
+                    res.status(400).json(result);
 			}
 		);
 	});
 });
 
 /**
+ * Get all tasks for a user
+ *
+ * URL: /api/person/
 * The GET request for all tasks for a user.
-* Required parameters: person_id(URL).
 */
 router.get('/person/:person_id', function(req, res) {
-
 	pool.getConnection(function(err, connection) {
-		if (!checkConnectionError(err, connection, res)) {return;}
-
+		if (!checkConnectionError(err, connection, res))
+		    return;
 		connection.query(
 			'SELECT * FROM todo LEFT JOIN todo_person USING(todo_id) WHERE todo_person.person_id = ?;',
-			[checkRange(req.params.person_id, 1, null)],
-			function(err, result) {
+			[checkRange(req.params.person_id, 1, null)], function(err, result) {
 				connection.release();
-				if (err) {throw err;}
-				if (result) {
-					var entries = [];
-					for (var i = 0; i < result.length; i++) {
-						var values = {};
-						for (var p in result[i]) {values[p] = result[i][p];}
-						delete values.person_id;
-						entries.push(values);
-					}
-					if (entries.length > 0) {res.status(200).json(entries);}
-					else {res.status(400).json(result);}
-				}
+				if (err)
+                    return res.status(500).send();
+                var entries = [];
+                for (var i = 0; i < result.length; i++) {
+                    var values = {};
+                    for (var p in result[i]) {values[p] = result[i][p];}
+                    delete values.person_id;
+                    entries.push(values);
+                }
+                if (entries.length > 0)
+                    res.status(200).json(entries);
+                else
+                    res.status(400).json(result);
 			}
 		);
 	});
 });
 
 /**
-* The PUT request for updating a task.
-* Required parameters: todo_id(URL).
-* Optional parameters: Any parameters that tasks use.
+ * Update a task
+ *
+ * URL: /api/tasks/{todo_id}
+ * method: PUT
+ * data: {
+ *      sql attribute style parameters to set value
+ * }
 */
 router.put('/:todo_id', function(req, res) {
-
 	pool.getConnection(function(err, connection) {
-		checkConnectionError(err, connection, res);
-
+        if (!checkConnectionError(err, connection, res))
+            return;
 		var query = putRequestSetup(checkRange(req.params.todo_id, 1, null), req.body, connection, "todo");
-		connection.query(
-			query[0],
-			query[1],
-			function(err, result) {checkResult(err, result, connection, res);}
-		);
+		connection.query(query[0], query[1], function(err, result) {
+		    checkResult(err, result, connection, res);
+		});
 	});
 });
 
 /**
-* The DELETE request for deleting a user from the task.
-* Required parameters: person_id(URL).
+ * Remove a person from task
+ *
+ * URL: /api/tasks/person/{todo_id}
+ * method: DELETE
+ * data: {
+ *      person_id
+ * }
 */
-router.delete('/person/:person_id', function(req, res) {
-
+router.delete('/person/:todo_id', function(req, res) {
 	pool.getConnection(function(err, connection) {
-		checkConnectionError(err, connection, res);
-
-		connection.query(
-			'DELETE FROM todo_person WHERE person_id = ? AND todo_id = ?',
-			[checkRange(req.params.person_id, 1, null), checkRange(req.body.todo_id, 1, null)],
-			function(err, result) {checkResult(err, result, connection, res);}
-		);
+        if (!checkConnectionError(err, connection, res))
+            return;
+		connection.query('DELETE FROM todo_person WHERE person_id = ? AND todo_id = ?',
+			[checkRange(req.body.person_id, 1, null), checkRange(req.params.todo_id, 1, null)],
+			function(err, result) {
+		    checkResult(err, result, connection, res);
+		});
 	});
 });
 
