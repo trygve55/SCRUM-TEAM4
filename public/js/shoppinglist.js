c6d0893c7b71e5aab2ec76563575d3427f23ec28
Dina Rosvoll <dina@dhcp-10-22-176-142.wlan.ntnu.no>
2018-01-19 14:35:54 +0100

    shoplist changes

--- public/js/shoppinglist.js
+++ public/js/shoppinglist.js
@@ -1,7 +1,8 @@
 var lang;
 var users = [];
+var lists = [];
 var curBudget;
-var list, balance, listItem, newListItem, popupTextList, popupList, balanceItem;
+var list, balance, listItem, newListItem, popupTextList, popupList, balanceItem, listReplace;
 
 $('document').ready(function () {
     //--------------Languages------------
@@ -54,7 +55,7 @@ $('document').ready(function () {
                         $(".fa-shopping-cart").html(" " + data["shop-buy"]);
                         $(".fa-users").html(" " + data["shop-share"]);
                         $(".fa-trash").html(" " + data["shop-delete"]);
-                        $(".fa-plus-circle").html(" " + data["shop-additem"]);
+                        $(".fa-plus-circle").html(" " + data["shop-add-item"]);
                         $(".list-name-input").attr('placeholder', data["shop-list-name-input"]);
 
                     }
@@ -89,7 +90,7 @@ $('document').ready(function () {
                         $(".fa-shopping-cart").html(" " + data["shop-buy"]);
                         $(".fa-users").html(" " + data["shop-share"]);
                         $(".fa-trash").html(" " + data["shop-delete"]);
-                        $(".fa-plus-circle").html(" " + data["shop-additem"]);
+                        $(".fa-plus-circle").html(" " + data["shop-add-item"]);
                         $(".list-name-input").attr('placeholder', data["shop-list-name-input"]);
                     }
                 });
@@ -108,7 +109,8 @@ $('document').ready(function () {
                 "listItem.html",
                 "newListItem.html",
                 "popupList.html",
-                "popupTextfieldList.html"
+                "popupTextfieldList.html",
+                "listReplace.html"
             ]
         },
         success: function(data){
@@ -119,6 +121,7 @@ $('document').ready(function () {
             popupTextList = Handlebars.compile(data["popupTextfieldList.html"]);
             popupList = Handlebars.compile(data["popupList.html"]);
             balanceItem = Handlebars.compile(data["balanceItem.html"]);
+            listReplace = Handlebars.compile(data["listReplace.html"]);
             prep();
         }
     });
@@ -129,9 +132,12 @@ function prep(){
         url: '/api/shoppingList/',
         method: 'GET',
         success: function(data){
+            lists = data;
             for(var j = 0; j < data.length; j++) {
+
                 var d = data[j];
                 var entries = "";
+                console.log(d.currency_short);
                 for (var i = 0; i < d.shopping_list_entries.length; i++) {
                     if(d.shopping_list_entries[i].purchased_by_person_id)
                         continue;
@@ -171,6 +177,7 @@ function prep(){
                     url: '/api/shoppingList/' + data.shopping_list_id,
                     method: 'GET',
                     success: function(data){
+                        lists.push(data);
                         $("#addlist").after(list({
                             shopping_list_id: data.shopping_list_id,
                             shopping_list_name: data.shopping_list_name,
@@ -187,137 +194,6 @@ function prep(){
                 });
             }
         });
-        /*
-
-        //-------------------Deletes the list------------------------
-        $("#shop-delete"+count).click(function () {
-=======
-    //---------------New list-------------
-    var count = 0;
-    $('#addlist').click(function () {
-        count++;
-        $("<div class=\"col-sm liste\" id='listenr"+count+"'><div class='row'>" +
-            "<div class='col-sm-7' style='text-align: left'>" +
-            "<div id='listname"+count+"' style='margin: 6px'><input type='text' class='form-control ni' id='listnameinput"+count+"'></div>" +
-            "<div id='nameforlist"+count+"'><h4>Liste"+count+"</h4></div></div>"+
-            "<div class='col-sm-5' style='text-align: right'>" +
-            "    <h4><i class=\"fa fa-circle\" aria-hidden=\"true\" style='color: white; text-shadow: 0px 0px 3px #000;' id='whitebutt"+count+"'></i>" +
-            "        <i class=\"fa fa-circle\" aria-hidden=\"true\" style='color: lightpink; text-shadow: 0px 0px 3px #000;' id='pinkbutt"+count+"'></i> " +
-            "        <i class=\"fa fa-circle\" aria-hidden=\"true\" style='color: #ffec8e; text-shadow: 0px 0px 3px #000;' id='yellowbutt"+count+"'></i> " +
-            "        <i class=\"fa fa-circle\" aria-hidden=\"true\" style='color: lightgreen; text-shadow: 0px 0px 3px #000;' id='greenbutt"+count+"'></i></h4></div>" +
-            "</div>" +
-            "<ul class=\"list-group\" id=\"itemlist"+count+"\"> </ul>"+
-            "<ul class=\"list-group\">"+
-            "<div class=\"inpi\" id=\"inputitem"+count+"\"><li class=\"list-group-item\" id=\"listitem"+count+"\">" +
-            "<input type=\"text\" class=\"form-control ni\" id=\"newitem"+count+"\">" +
-            "</li></div><li class=\"list-group-item addi\" id=\"additem"+count+"\"><i class=\"fa fa-plus-circle\" aria-hidden=\"true\"> "+lang["shop-additem"]+"</i></li>" +
-            "</ul><i class=\"fa fa-shopping-cart\" aria-hidden=\"true\" id='shoppingcart"+count+"' style='font-size: 1.8vh'> "+lang["shop-buy"]+"</i>" +
-            "<i class=\"fa fa-users\" aria-hidden=\"true\" style='font-size: 1.8vh' id='shop-members"+count+"'> "+lang["shop-share"]+"</i>" +
-            "<i class=\"fa fa-trash\" aria-hidden=\"true\" id='shop-delete"+count+"' style='font-size: 1.8vh'> "+lang["shop-delete"]+"</i><i class=\"fa fa-money\" style='font-size: 1.8vh' aria-hidden=\"true\" id='shop-balance"+count+"'> "+lang["shop-balance"]+"</i></div>" +
-            "<div class=\"messagepop pop\" id=\"shop-popup\">" +
-            "   <p align=\"center\" style=\"font-size: 20px\">"+lang["shop-addmemberstolist"]+"</p>" +
-            "   <ul class=\"list-group\" id='shop-memberslist"+count+"' style='margin-top: 10px; margin-bottom: 10px; max-height: 30vh;'></ul>" +
-            "   <div class=\"row\">" +
-            "       <div class=\"col-md-8\" id=\"scrollable-dropdown-menu\"><input class=\"typeahead form-control\" id=\"shop-member"+count+"\"></div>" +
-            "       <div class=\"col-md-1\"><button type=\"button\" class=\"btn btn-info\" id=\"shop-adduser"+count+"\">"+lang["shop-adduser"]+"</button></div>" +
-            "   </div>" +
-            "   <div style=\"margin-top: 5%\"><button align=\"right\" type=\"button\" class=\"btn\" id=\"shop-cancel"+count+"\" style=\"background-color: lightgrey\">"+lang["shop-cancel"]+"</button>" +
-            "</div></div>"+
-            "<div class='col-sm liste' id='balancediv"+count+"'>" +
-            "<h4>Balance</h4><table class='table table-hover table-bordered'><thead><tr><th>Shoppingtrip</th><th>Price</th></tr></thead>" +
-            "<tbody><tr><td>Testtrip</td><td>5mill</td></tr></tbody>"+
-            "</table><i class=\"fa fa-list\" aria-hidden=\"true\" id='shop-backlist"+count+"'> "+lang["shop-backlist"]+"</i>").insertAfter("#addlist");
-        $('#listname'+count).hide();
-        $('#balancediv'+count).hide();
-        var itemcount = 0;
-
-        //-------------Edit listname------------------
-        $('#nameforlist'+count).click(function(){
-            var navn = this.id;
-            var ide = navn.split("t").pop();
-            console.log(ide + " name for list clicked");
-            $('#nameforlist'+ide).hide();
-            $('#listname'+ide).show();
-            $('#listnameinput'+ide).focus();
->>>>>>> tasklist
-
-            var navn = this.id;
-            var ide = navn.split("e").pop();
-            $("#shop-delete"+ide).html(" Press again to delete");
-            $("#shop-delete"+count).click(function () {
-                $('#listenr'+ide).remove();
-            });
-        });
-
-<<<<<<< HEAD
-        //-----------------Opens member popup--------------------
-=======
-        //---------------------Listcolor----------------------
-        $('#pinkbutt'+count).click(function () {
-            var navn = this.id;
-            var ide = navn.split("t").pop(); //listeid
-            var name = "listenr"+ide;
->>>>>>> tasklist
-
-        $('#shop-members'+count).on('click', function() {
-            console.log("clicked");
-            $(this).addClass('selected');
-            $('.pop').slideFadeToggle();
-        });
-
-        $('#shop-cancel'+count).on('click', function() {
-            deselect($('#contact'));
-        });
-
-        $.fn.slideFadeToggle = function(easing, callback) {
-            return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
-        };
-
-        $.ajax({
-            'url': '/api/user/all',
-            method: "GET",
-            success: function(data){
-                var cnt = [];
-                for(var i = 0; i < data.length; i++){
-                    var u = {
-                        id: data[i].person_id,
-                        name: data[i].forename + " " + (data[i].middlename ? data[i].middlename + " " : "") + data[i].lastname,
-                        username: data[i].username
-                    };
-                    users.push(u);
-                    cnt.push(u.name);
-                }
-                $('#scrollable-dropdown-menu .typeahead').typeahead(null, {
-                    name: 'users',
-                    limit: 10,
-                    source: new Bloodhound({
-                        datumTokenizer: Bloodhound.tokenizers.whitespace,
-                        queryTokenizer: Bloodhound.tokenizers.whitespace,
-                        prefetch: '/api/user/all?slim=1'
-                    })
-                });
-            },
-            error: console.error
-        });
-
-        $('#shop-member'+count).keypress(function(event) {
-            if(event.keyCode == 13 || event.which == 13){
-                var navn = this.id;
-                var ide = navn.split("r").pop(); //listeid
-                addmember(ide);
-            }
-        });
-
-<<<<<<< HEAD
-        $('#shop-adduser'+count).click(function(){
-=======
-        //-----------------Press add item button opens inputfield--------------
-        $('#additem'+count).click(function () {
->>>>>>> tasklist
-            var navn = this.id;
-            var ide = navn.split("r").pop(); //listeid
-            addmember(ide);
-        });*/
     });
 }
 
@@ -331,7 +207,7 @@ function setupClicks(){
         $(div).children(".list-name-input").val(title).focus();
     });
 
-    $(".list-name-input").unbind("focusout").focusout(function(){
+    /*$(".list-name-input").unbind("focusout").focusout(function(){
         var text = $(this).val();
         var id = $(this).closest("div[data-id]").data("id");
         var h4 = $(this).parent().parent().children(".list-name");
@@ -344,24 +220,29 @@ function setupClicks(){
             data: {
                 shopping_list_name: text
             }
-        });});
+        });});*/
 
     $(".list-name-input").unbind("keypress").keypress(function(e){
         if(e.keyCode != 13 && e.which != 13)
             return;
         var text = $(this).val();
-        var id = $(this).closest("div[data-id]").data("id");
-        var h4 = $(this).parent().parent().children(".list-name");
-        $(h4).html(text);
-        $(this).parent().hide();
-        $(h4).show();
-        $.ajax({
-            url: '/api/shoppingList/' + id,
-            method: 'PUT',
-            data: {
-                shopping_list_name: text
-            }
-        });
+        if(text === ""){
+            $(this).parent().hide();
+            $(this).parent().parent().children(".list-name").show();
+        }else{
+            var id = $(this).closest("div[data-id]").data("id");
+            var h4 = $(this).parent().parent().children(".list-name");
+            $(h4).html(text);
+            $(this).parent().hide();
+            $(h4).show();
+            $.ajax({
+                url: '/api/shoppingList/' + id,
+                method: 'PUT',
+                data: {
+                    shopping_list_name: text
+                }
+            });
+        }
     });
 
     $(".add-item").unbind("click").click(function(){
@@ -395,57 +276,60 @@ function setupClicks(){
             $(this).closest("li").remove();
         }).focus();
     });
-
-    $('.pink-select').unbind("click").click(function () {
-        var ls = $(this).closest("div[data-id]");
-        var id = $(ls).css('background-color', $(this).data('color')).data("id");
-        $.ajax({
-            url: '/api/shoppingList/' + id,
-            method: 'PUT',
-            data: {
-                color_hex: parseInt(rgb2hex($(ls).css('background-color')).split("#")[1], 16)
-            }
+    function colorRefresh() {
+        $('.pink-select').unbind("click").click(function () {
+            var ls = $(this).closest("div[data-id]");
+            var id = $(ls).css('background-color', $(this).data('color')).data("id");
+            $.ajax({
+                url: '/api/shoppingList/' + id,
+                method: 'PUT',
+                data: {
+                    color_hex: parseInt(rgb2hex($(ls).css('background-color')).split("#")[1], 16)
+                }
+            });
         });
-    });
 
-    $('.yellow-select').unbind("click").click(function () {
-        var ls = $(this).closest("div[data-id]");
-        var id = $(ls).css('background-color', $(this).data('color')).data("id");
-        $.ajax({
-            url: '/api/shoppingList/' + id,
-            method: 'PUT',
-            data: {
-                color_hex: parseInt(rgb2hex($(ls).css('background-color')).split("#")[1], 16)
-            }
+        $('.yellow-select').unbind("click").click(function () {
+            var ls = $(this).closest("div[data-id]");
+            var id = $(ls).css('background-color', $(this).data('color')).data("id");
+            $.ajax({
+                url: '/api/shoppingList/' + id,
+                method: 'PUT',
+                data: {
+                    color_hex: parseInt(rgb2hex($(ls).css('background-color')).split("#")[1], 16)
+                }
+            });
         });
-    });
 
-    $('.green-select').unbind("click").click(function () {
-        var ls = $(this).closest("div[data-id]");
-        var id = $(ls).css('background-color', $(this).data('color')).data("id");
-        $.ajax({
-            url: '/api/shoppingList/' + id,
-            method: 'PUT',
-            data: {
-                color_hex: parseInt(rgb2hex($(ls).css('background-color')).split("#")[1], 16)
-            }
+        $('.green-select').unbind("click").click(function () {
+            var ls = $(this).closest("div[data-id]");
+            var id = $(ls).css('background-color', $(this).data('color')).data("id");
+            $.ajax({
+                url: '/api/shoppingList/' + id,
+                method: 'PUT',
+                data: {
+                    color_hex: parseInt(rgb2hex($(ls).css('background-color')).split("#")[1], 16)
+                }
+            });
         });
-    });
 
-    $('.white-select').unbind("click").click(function () {
-        var ls = $(this).closest("div[data-id]");
-        var id = $(ls).css('background-color', $(this).data('color')).data("id");
-        $.ajax({
-            url: '/api/shoppingList/' + id,
-            method: 'PUT',
-            data: {
-                color_hex: parseInt(rgb2hex($(ls).css('background-color')).split("#")[1], 16)
-            }
+        $('.white-select').unbind("click").click(function () {
+            var ls = $(this).closest("div[data-id]");
+            var id = $(ls).css('background-color', $(this).data('color')).data("id");
+            $.ajax({
+                url: '/api/shoppingList/' + id,
+                method: 'PUT',
+                data: {
+                    color_hex: parseInt(rgb2hex($(ls).css('background-color')).split("#")[1], 16)
+                }
+            });
         });
-    });
+    }
+    colorRefresh();
 
     $(".fa-money").unbind("click").click(function(){
         var id = $(this).closest("div[data-id]").data("id");
+        var mbutton = this;
         $.ajax({
             url: '/api/budget/' + id,
             method: 'GET',
@@ -456,10 +340,9 @@ function setupClicks(){
                 for(var i = 0; i < data.budget_entries.length; i++){
                     entries += "<tr data-id='" + data.budget_entries[i].budget_entry_id + "'><td>" + data.budget_entries[i].entry_datetime + "</td><td>" + data.budget_entries[i].amount + "</td>";
                 }
-                $("body").append(balance({
+                $(mbutton).closest("div[data-id]").html(balance({
                     title: lang["shop-balance"],
                     complete: lang["shop-ok"],
-                    data: "data-id='" + id + "'",
                     lang_trip: lang["shop-trip"],
                     lang_price: lang["shop-price"],
                     budget_entries: entries
@@ -489,11 +372,44 @@ function setupClicks(){
                 });
 
                 $('#popup-complete').click(function(){
-                    $(this).closest(".pop").remove();
+                    var entries = "";
+                    console.log(lists);
+                    console.log(id);
+                    for(var j = 0; j < lists.length; j++) {
+                        if(lists[j].shopping_list_id != id)
+                            continue;
+                        var d = lists[j];
+                        for (var i = 0; i < d.shopping_list_entries.length; i++) {
+                            if (d.shopping_list_entries[i].purchased_by_person_id)
+                                continue;
+                            entries += listItem({
+                                entry_text: d.shopping_list_entries[i].entry_text,
+                                entry_id: d.shopping_list_entries[i].shopping_list_entry_id
+                            });
+                        }
+                        break;
+                    }
+                    $(this).closest("div[data-id]").html(listReplace({
+                        shopping_list_id: d.shopping_list_id,
+                        shopping_list_name: (d.shopping_list_name == "" ? lang["shop-list-name-input"] : d.shopping_list_name),
+                        shopping_list_entries: entries,
+                        lang_add_item: lang["shop-add-item"],
+                        lang_buy_items: lang["shop-buy"],
+                        lang_share_members: lang["shop-share"],
+                        lang_delete_list: lang["shop-delete"],
+                        lang_settlement: lang["shop-balance"],
+                        color_hex: (d.color_hex ? d.color_hex.toString(16) : "FFFFFF")
+                    }));
+                    setupClicks();
+                    colorRefresh();
                 });
             },
             error: console.error
         });
+    });
+
+    $(".fa-trash").unbind("click").click(function () {
+       $(this).closest("div[data-id]").remove();
     });
 
     $(".fa-shopping-cart").unbind("click").click(function(){
