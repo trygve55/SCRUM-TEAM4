610460ecdfc08c1ede62d7f2cad93f43946cdbb2
emkristi <emkristi@stud.ntnu.no>
2018-01-22 13:28:21 +0100

    added js

--- public/js/forgotPassword.js
+++ public/js/forgotPassword.js
@@ -1,3 +1,19 @@
 /**
  * Created by Eline on 15.01.2018.
  */
+$('document').ready(function () {
+    $('#passwordbutton').click(function () {
+        $.ajax({
+
+            url: '/api/user/forgottenPasswordReset',
+            method: 'POST',
+            data: {
+                email: ''
+            },
+            success: function (data){
+                console.log(data)
+            },
+            error: console.error
+        });
+    });
+});
