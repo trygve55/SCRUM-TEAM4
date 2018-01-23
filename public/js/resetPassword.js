aa7443fafcb8dc56d602bf8a8a25ec9570f9d039
emkristi <emkristi@stud.ntnu.no>
2018-01-23 09:07:13 +0100

    added language

--- public/js/resetPassword.js
+++ public/js/resetPassword.js
@@ -1,8 +1,86 @@
 /**
  * Created by Eline on 22.01.2018.
  */
+
+var lang;
+
 $(document).ready(function () {
 
+    $.ajax({
+        url: '/api/language',
+        method: 'GET',
+        data: {
+            lang: 'nb_NO',
+            path: window.location.pathname
+        },
+        success: function (data) {
+            lang = data;
+            for (var p in data) {
+                if (data.hasOwnProperty(p)) {
+                    $("#" + p).html(data[p]);
+                }
+            }
+        }
+    });
+
+    $('#profile-norway').click(function () {
+        $.ajax({
+            url: '/api/language',
+            method: 'POST',
+            data: {
+                lang: 'nb_NO'
+            },
+            success: function () {
+                $.ajax({
+                    url: '/api/language',
+                    method: 'GET',
+                    data: {
+                        lang: 'nb_NO',
+                        path: window.location.pathname
+                    },
+                    success: function (data) {
+                        lang = data;
+                        for (var p in data) {
+                            if (data.hasOwnProperty(p)) {
+                                $("#" + p).html(data[p]);
+                            }
+                        }
+                    }
+                });
+            },
+            error: console.error
+        });
+
+    });
+
+    $('#profile-england').click(function () {
+        $.ajax({
+            url: '/api/language',
+            method: 'POST',
+            data: {
+                lang: 'en_US'
+            },
+            success: function () {
+                $.ajax({
+                    url: '/api/language',
+                    method: 'GET',
+                    data: {
+                        lang: 'en_US',
+                        path: window.location.pathname
+                    },
+                    success: function (data) {
+                        lang = data;
+                        for (var p in data) {
+                            if (data.hasOwnProperty(p)) {
+                                $("#" + p).html(data[p]);
+                            }
+                        }
+                    }
+                });
+            },
+            error: console.error
+        });
+    });
 
     $('#resetbutton').click(function () {
         $.ajax({
