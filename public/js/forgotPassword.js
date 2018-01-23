370ddd537ead2c30fb4ae80a6468514d6b993e63
emkristi <emkristi@stud.ntnu.no>
2018-01-23 08:33:26 +0100

    fix

--- public/js/forgotPassword.js
+++ public/js/forgotPassword.js
@@ -2,13 +2,93 @@
  * Created by Eline on 15.01.2018.
  */
 $('document').ready(function () {
-    $('#passwordbutton').click(function () {
-        $.ajax({
+    /* Language */
 
-            url: '/api/user/forgottenPasswordReset',
+    /*
+    $.ajax({
+        url: '/api/language',
+        method: 'GET',
+        data: {
+            lang: 'nb_NO',
+            path: window.location.pathname
+        },
+        success: function (data) {
+            lang = data;
+            for (var p in data) {
+                if (data.hasOwnProperty(p)) {
+                    $("#" + p).html(data[p]);
+                }
+            }
+        }
+    });
+
+    $('#profile-norway').click(function () {
+        $.ajax({
+            url: '/api/language',
             method: 'POST',
             data: {
-                email: ''
+                lang: 'nb_NO'
+            },
+            success: function () {
+                $.ajax({
+                    url: '/api/language',
+                    method: 'GET',
+                    data: {
+                        lang: 'nb_NO',
+                        path: window.location.pathname
+                    },
+                    success: function (data) {
+                        lang = data;
+                        for (var p in data) {
+                            if (data.hasOwnProperty(p)) {
+                                $("#" + p).html(data[p]);
+                            }
+                        }
+                    }
+                });
+            },
+            error: console.error
+        });
+
+    });
+
+    $('#profile-england').click(function () {
+        $.ajax({
+            url: '/api/language',
+            method: 'POST',
+            data: {
+                lang: 'en_US'
+            },
+            success: function () {
+                $.ajax({
+                    url: '/api/language',
+                    method: 'GET',
+                    data: {
+                        lang: 'en_US',
+                        path: window.location.pathname
+                    },
+                    success: function (data) {
+                        lang = data;
+                        for (var p in data) {
+                            if (data.hasOwnProperty(p)) {
+                                $("#" + p).html(data[p]);
+                            }
+                        }
+                    }
+                });
+            },
+            error: console.error
+        });
+    });
+
+    */
+
+    $('#resetbut').click(function () {
+        $.ajax({
+            url: '/api/user/forgottenPasswordEmail',
+            method: 'POST',
+            data: {
+                email: $('#emailadr').val()
             },
             success: function (data){
                 console.log(data)
