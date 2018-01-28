ee5af2b83dc8ffa220b2a6eb214fdaa6643bd481
Trygve Nerland <trygvne@stud.ntnu.no>
2018-01-28 20:07:24 +0100

    shopping list fix

--- public/js/group.js
+++ public/js/group.js
@@ -3,7 +3,7 @@ var stTransparent = "0.5",
 	statColours = [["(0, 30, 170, " + stTransparent + ")", "(0, 0, 132, 1)"], ["(170, 30, 0, " + stTransparent + ")", "(132, 0, 0, 1)"]],
 	statLabels = ["Income", "Expenses"];
 const MILLIS_DAY = 86400000;
-var activeTab = "feed", currentGroup, listItem, newListItem, balance, balanceItem, popupTextList, currentShoppingList, feedPost, readMore, taskItem, dataTask = [], taskItemDone, popupAssign, listMemebers, listMemebersAdmin, adminView, adminViewSimple, groupShopping;
+var activeTab = "feed", currentGroup, listItem, newListItem, balance, balanceItem, popupTextList, currentShoppingList, feedPost, readMore, taskItem, dataTask = [], taskItemDone, popupAssign, listMemebers, listMemebersAdmin, adminView, adminViewSimple, groupShopping, popupSettle;
 var statColours = [["(0, 30, 170, 0.5)", "(0, 0, 132, 1)"], ["(170, 30, 0, 0.5)", "(132, 0, 0, 1)"]], statLabels = ["Income", "Expenses"];
 var generalLabels, grouplist;
 var lang;
@@ -132,6 +132,7 @@ $(function() {
                 'listMemebers.html',
                 'listMemebersAdmin.html',
                 'adminView.html',
+                "popupSettle.html",
                 'adminViewSimple.html',
                 'groupShopping.html'
             ]
@@ -148,6 +149,7 @@ $(function() {
             taskItemDone = Handlebars.compile(data['taskItemDone.html']);
             popupAssign = Handlebars.compile(data['popupAssign.html']);
             listMemebers = Handlebars.compile(data['listMemebers.html']);
+            popupSettle = Handlebars.compile(data["popupSettle.html"]);
             listMemebersAdmin = Handlebars.compile(data['listMemebersAdmin.html']);
             adminView = Handlebars.compile(data['adminView.html']);
             adminViewSimple = Handlebars.compile(data['adminViewSimple.html']);
@@ -636,8 +638,9 @@ function setupClicks(){
                 $('.balancelist').unbind("click").click(function () {
                     var name = this.innerHTML.split('>')[1].split('<')[0];
                     for(var j=0; j<balancelist.length; j++){
-                        var thefullname = balancelist[j].forename + " " + balancelist[j].lastname;
-                        var personid = balancelist[j].person_id;
+                        let thefullname = balancelist[j].forename + " " + balancelist[j].lastname;
+                        let personid = balancelist[j].person_id;
+                        let row = $(this);
                         if(thefullname == name){
                             if(balancelist[j].amount <= 0) {
                                 break;
@@ -667,6 +670,7 @@ function setupClicks(){
                                     }),
                                     error: console.error,
                                     success: function (data) {
+                                        row.remove();
                                         $(suc).closest('.pop').remove();
                                     }
                                 })
